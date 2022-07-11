1073dba643298a87eb89e75cdbf11645df110818
ray <mora.r98@gmail.com>
2022-07-11 16:43:38 -0400

    fixing url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="ds5100-hw14",
       version=0.1,
       description="Answers to life's deepest question",
-      url="https://github.com/raymundo-mora/my_first_python_package",
+      url="https://github.com/raymundo-mora/ds5100-hw14",
       author='Raymundo Mora',
       author_email='rm3xw@virginia.edu',
       license='MIT',
